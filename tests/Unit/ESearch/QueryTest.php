6ba70cbe66e064081c23a4712082e7936f6a3e95
Lars Nieuwenhuizen <l.nieuwenhuizen@developers.nl>
2018-05-11 14:00:46 +0200

    TASK: Add travis and unit test configuration

---
+++ tests/Unit/ESearch/QueryTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LarsNieuwenhuizen\EUtilities\Esearch\Tests\ESearch;
+
+use PHPUnit\Framework\TestCase;
+
+class QueryTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function dummyTest()
+    {
+        $this->assertTrue(true);
+    }
+}
